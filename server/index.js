2357f7d70a515f066ec20d38726e984402f52345
Sondro <memoiran@hotmail.com>
2018-01-18 19:20:48 -0800

    commit

--- server/index.js
+++ server/index.js
@@ -16,38 +16,38 @@ io.on('connection', socket => {
     players[socket.id] = state
     // Emit the update-players method in the client side
     io.emit('update-players', players)
-  })
+  });
 
   socket.on('disconnect', state => {
-    delete players[socket.id]
+    delete players[socket.id];
     io.emit('update-players', players)
-  })
+  });
 
   // When a player moves
   socket.on('move-player', data => {
-    const { x, y, angle, playerName, speed } = data
+    const { x, y, angle, playerName, speed } = data;
 
     // If the player is invalid, return
     if (players[socket.id] === undefined) {
-      return
+      return;
     }
 
     // Update the player's data if he moved
-    players[socket.id].x = x
-    players[socket.id].y = y
-    players[socket.id].angle = angle
+    players[socket.id].x = x;
+    players[socket.id].y = y;
+    players[socket.id].angle = angle;
     players[socket.id].playerName = {
       name: playerName.name,
       x: playerName.x,
-      y: playerName.y
-    }
+      y: playerName.y,
+    };
     players[socket.id].speed = {
       value: speed.value,
       x: speed.x,
-      y: speed.y
-    }
+      y: speed.y,
+    };
 
     // Send the data back to the client
-    io.emit('update-players', players)
-  })
-})
+    io.emit('update-players', players);
+  });
+});
