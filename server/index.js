0843bde4e639f26ea14f0bf882d8e88d049f2a92
Sondro <memoiran@hotmail.com>
2018-01-18 19:50:05 -0800

    commit

--- server/index.js
+++ server/index.js
@@ -1,26 +1,26 @@
 'use strict'
-const http = require('http')
-const app = require('./config')
-const Server = http.Server(app)
-const PORT = process.env.PORT || 8000
-const io = require('socket.io')(Server)
+const http = require('http');
+const app = require('./config');
+const Server = http.Server(app);
+const PORT = process.env.PORT || 8000;
+const io = require('socket.io')(Server);
 
 Server.listen(PORT, () => console.log('Game server running on:', PORT))
 
-const players = {}
+const players = {};
 
 io.on('connection', socket => {
   // When a player connects
   socket.on('new-player', state => {
-    console.log('New player joined with state:', state)
-    players[socket.id] = state
+    console.log('New player joined with state:', state);
+    players[socket.id] = state;
     // Emit the update-players method in the client side
-    io.emit('update-players', players)
+    io.emit('update-players', players);
   });
 
   socket.on('disconnect', state => {
     delete players[socket.id];
-    io.emit('update-players', players)
+    io.emit('update-players', players);
   });
 
   // When a player moves
